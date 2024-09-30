3e84eb68c12be315956f4cd727a872237b5c795a
jorgebp226 <jorgebp226@gmail.com>
2024-09-30 14:20:21 +0200

    Mejoras en la autentificación

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 // src/App.js
 import React from 'react';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css'; // Estilos por defecto para el componente Authenticator
 import Calendar from './components/Calendar';
 import ProgressChart from './components/ProgressChart';
 import Visualization from './components/Visualization';
@@ -7,12 +9,18 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="app-container">
-      <h1>Seguimiento de Virtudes</h1>
-      <Calendar />
-      <ProgressChart />
-      <Visualization />
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div className="app-container">
+          <h1>Seguimiento de Virtudes</h1>
+          <p>Bienvenido, {user.username}</p>
+          <button onClick={signOut}>Cerrar sesión</button>
+          <Calendar />
+          <ProgressChart />
+          <Visualization />
+        </div>
+      )}
+    </Authenticator>
   );
 };
 
